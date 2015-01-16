1542039eef7e7b32c64ddc987ff50af139c0dace
Nicolas Siver <nicolas@siver.im>
2015-01-17 00:11:18 +0200

    Alpha control

--- main.py
+++ main.py
@@ -11,20 +11,30 @@ class ColorApp(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('./ui/mainWindow.ui', self)
-        self.onChangedRgb(0)
+        self.onChange(0)
         self.addListeners()
 
     def addListeners(self):
-        self.uiRedInput.valueChanged.connect(self.onChangedRgb)
-        self.uiGreenInput.valueChanged.connect(self.onChangedRgb)
-        self.uiBlueInput.valueChanged.connect(self.onChangedRgb)
+        self.uiRedInput.valueChanged.connect(self.onChange)
+        self.uiGreenInput.valueChanged.connect(self.onChange)
+        self.uiBlueInput.valueChanged.connect(self.onChange)
+        self.uiAlphaBox.toggled.connect(self.onChange)
+        self.uiAlphaSlider.valueChanged.connect(self.onChange)
 
-    def onChangedRgb(self, value):
+    def onChange(self, value):
         self.updateHex()
         self.updateOutput()
 
     def updateOutput(self):
-        pass
+        output = 'rgb({}, {}, {})'
+        if self.uiAlphaBox.isChecked():
+            output = 'rgba({}, {}, {}, {:.2})'
+        self.uiLabelRgbaResult.setText(
+            output.format(
+                self.uiRedInput.value(),
+                self.uiGreenInput.value(),
+                self.uiBlueInput.value(),
+                self.uiAlphaSlider.value() * 0.01))
 
     def updateHex(self):
         rgb = (self.uiRedInput.value(), self.uiGreenInput.value(), self.uiBlueInput.value())
